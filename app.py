ccea28e1218389c5e920815bc694cec1e4a3a8fa
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-02-26 12:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template, request, redirect, session
 import random
 import os
 import psycopg2
+from urllib.parse import urlparse
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY", "fallbackkey")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
 
 def init_db():
     conn = get_connection()
@@ -51,20 +52,26 @@ def login():
 
     return render_template("login.html")
 
+
 @app.route("/verify", methods=["GET", "POST"])
 def verify():
     if request.method == "POST":
-        entered = request.form["otp"]
+        entered_otp = request.form["otp"]
 
-        if entered == session.get("otp"):
+        if entered_otp == session.get("otp"):
+            session["logged_in"] = True
             return redirect("/admin")
         else:
             return "Invalid OTP"
 
     return render_template("verify.html")
 
+
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
+    if not session.get("logged_in"):
+        return redirect("/")
+
     if request.method == "POST":
         mobile = request.form["mobile"]
 
@@ -80,12 +87,13 @@ def admin():
 
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("SELECT mobile FROM users;")
+    cur.execute("SELECT mobile FROM users ORDER BY id DESC;")
     mobiles = cur.fetchall()
     cur.close()
     conn.close()
 
     return render_template("admin.html", mobiles=mobiles)
+
 
 if __name__ == "__main__":
     app.run()
